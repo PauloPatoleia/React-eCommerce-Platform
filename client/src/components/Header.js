fb9711233d02be462593321945094b7e55b1e470
vmpatoleia@protonmail.com <vmpatoleia@protonmail.com>
2020-12-22 19:34:16 +0000

    Build header mobile menus

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,55 +1,196 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { Container, Grid } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import { Link } from "react-router-dom";
+import { Container } from "@material-ui/core/";
 import PersonIcon from "@material-ui/icons/Person";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  grow: {
     flexGrow: 1,
-    marginBottom: 50,
   },
   menuButton: {
-    flex: 1,
+    marginRight: theme.spacing(2),
   },
   title: {
-    padding: 0,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "rgba(0, 0, 0, 0.04)", //fade(theme.palette.common.white, 0.15)
+    "&:hover": {
+      backgroundColor: "rgba(0, 0, 0, 0.08)", //fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
   },
 }));
 
 export default function Header() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem component={Link} to={"/products/1"}>
+        <Button color="inherit" startIcon={<LockOpenIcon />}>
+          Sign in
+        </Button>
+      </MenuItem>
+      <MenuItem component={Link} to={"/products/1"}>
+        <Button color="inherit" startIcon={<PersonIcon />}>
+          Login
+        </Button>
+      </MenuItem>
+      <MenuItem component={Link} to={"/products/1"}>
+        <Button color="inherit" startIcon={<ShoppingCartIcon />}>
+          Cart
+        </Button>
+      </MenuItem>
+    </Menu>
+  );
+
   return (
-    <header className={classes.root}>
-      <AppBar color="transparent" position="static">
+    <div className={classes.grow}>
+      <AppBar position="static" color="transparent">
         <Container fixed>
-          <Grid container direction="row" alignItems="center">
-            <Grid item xs={4}>
-              <Grid alignItems="center" container justify="flex-start">
-                <Toolbar className={classes.title}>
-                  <Typography variant="h6">LOGO</Typography>
-                </Toolbar>
-              </Grid>
-            </Grid>
-            <Grid item xs={8}>
-              <Grid container justify="flex-end" className={classes.menuButton}>
+          <Toolbar>
+            <MenuItem component={Link} to={"/"}>
+              <Typography className={classes.title} variant="h6" noWrap>
+                LOGO
+              </Typography>
+            </MenuItem>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+
+            <div className={classes.sectionDesktop}>
+              <MenuItem component={Link} to={"/products/1"}>
+                <Button color="inherit" startIcon={<LockOpenIcon />}>
+                  Sign in
+                </Button>
+              </MenuItem>
+              <MenuItem component={Link} to={"/products/1"}>
                 <Button color="inherit" startIcon={<PersonIcon />}>
+                  Login
+                </Button>
+              </MenuItem>
+              <MenuItem component={Link} to={"/products/1"}>
+                <Button color="inherit" startIcon={<ShoppingCartIcon />}>
                   Cart
                 </Button>
-                <Button color="inherit" startIcon={<ShoppingCartIcon />}>
-                  Sign In
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
+              </MenuItem>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
         </Container>
       </AppBar>
-    </header>
+      {renderMobileMenu}
+    </div>
   );
 }
