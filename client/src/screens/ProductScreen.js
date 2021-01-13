d2cc7e9acf3e2b624053dd15850d0859b783708d
vmpatoleia@protonmail.com <vmpatoleia@protonmail.com>
2021-01-13 13:21:58 +0000

    Fetch product from back-end

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,33 +1,25 @@
-import React from "react";
-import { Container, Grid, Typography } from "@material-ui/core";
-import { Divider } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Grid, Typography } from "@material-ui/core";
 import ReadOnlyStarRating from "../components/ReadOnlyStarRating";
-import Box from "@material-ui/core/Box";
-import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import Link from "@material-ui/core/Link";
+import axios from "axios";
 
-export default function ProductScreen() {
-  var product = {
-    _id: "1",
-    name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
-    description:
-      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
-    brand: "Apple",
-    category: "Electronics",
-    price: 89.99,
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 12,
-  };
+export default function ProductScreen({ match }) {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/products/${match.params.id}`).then(function (response) {
+      setProduct(response.data);
+    });
+  }, []);
 
   return (
     <>
       <Link href="/" underline="none" color="inherit">
         <div style={{ display: "flex", marginBottom: "20px" }}>
-          <ArrowBackIosIcon color="default" />
+          <ArrowBackIosIcon />
           <Typography
             variant="subtitle1"
             display="block"
@@ -50,7 +42,6 @@ export default function ProductScreen() {
           <img
             style={{ maxHeight: "100%", maxWidth: "100%" }}
             src="https://www.notebookcheck.net/uploads/tx_nbc2/4_zu_3_teaser_05.jpg"
-            fluid
           />
         </Grid>
 
@@ -84,7 +75,7 @@ export default function ProductScreen() {
           </Typography>
           <br></br>
 
-          <Button variant="contained" color="defalt" disableElevation>
+          <Button variant="contained" color="default" disableElevation>
             Add to Cart
           </Button>
         </Grid>
