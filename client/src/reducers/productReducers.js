11fcaf5a290cf2f5b7e3596713dc124fc014126c
vmpatoleia@protonmail.com <vmpatoleia@protonmail.com>
2021-04-03 22:27:55 +0100

    Create Product Details Reducer & Action

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -2,19 +2,40 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstants";
 
-const initialState = {
-  products: [],
-};
-
-export const productListReducer = (state = initialState, { type, payload }) => {
+export const productListReducer = (
+  state = {
+    products: [],
+  },
+  { type, payload }
+) => {
   switch (type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: payload };
+
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  { type, payload }
+) => {
+  switch (type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, product: { product: { reviews: [] } } };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: payload };
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: payload };
 
     default:
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -3,17 +3,22 @@ import { Grid, Typography } from "@material-ui/core";
 import ReadOnlyStarRating from "../components/ReadOnlyStarRating";
 import Button from "@material-ui/core/Button";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProduct } from "../actions/productDetails";
 
 export default function ProductScreen({ match }) {
-  const [product, setProduct] = useState([]);
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  console.log(productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    axios.get(`/api/products/${match.params.id}`).then(function (response) {
-      setProduct(response.data);
-    });
-  }, [match]);
+    dispatch(fetchProduct(match.params.id));
+  }, [dispatch, match]);
 
   return (
     <>
@@ -31,55 +36,61 @@ export default function ProductScreen({ match }) {
         </div>
       </Button>
 
-      <Grid
-        container
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-        spacing={4}
-      >
-        <Grid item lg={6}>
-          <img
-            style={{ maxHeight: "100%", maxWidth: "100%" }}
-            src="https://www.notebookcheck.net/uploads/tx_nbc2/4_zu_3_teaser_05.jpg"
-          />
-        </Grid>
-
-        <Grid item lg={4}>
-          <Typography style={{ marginBottom: 30 }} variant="h6" gutterBottom>
-            {product.name}
-          </Typography>
-          <Typography variant="body1" display="block" gutterBottom>
-            Price: {product.price}$
-          </Typography>
-
-          <Grid
-            container
-            direction="row"
-            justify="flex-start"
-            alignItems="center"
-          >
-            <Grid style={{ marginRight: "10px" }} item xs={12} sm={3}>
-              <ReadOnlyStarRating value={product.rating} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Typography variant="caption" display="block" gutterBottom>
-                {product.numReviews} reviews
-              </Typography>
-            </Grid>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message severity="error" message={error} />
+      ) : (
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+          spacing={4}
+        >
+          <Grid item lg={6}>
+            <img
+              style={{ maxHeight: "100%", maxWidth: "100%" }}
+              src="https://www.notebookcheck.net/uploads/tx_nbc2/4_zu_3_teaser_05.jpg"
+            />
           </Grid>
-          <br></br>
 
-          <Typography variant="body2" display="block" gutterBottom>
-            {product.description}
-          </Typography>
-          <br></br>
+          <Grid item lg={4}>
+            <Typography style={{ marginBottom: 30 }} variant="h6" gutterBottom>
+              {product.name}
+            </Typography>
+            <Typography variant="body1" display="block" gutterBottom>
+              Price: {product.price}$
+            </Typography>
 
-          <Button variant="contained" color="default" disableElevation>
-            Add to Cart
-          </Button>
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="center"
+            >
+              <Grid style={{ marginRight: "10px" }} item xs={12} sm={3}>
+                <ReadOnlyStarRating value={product.rating} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Typography variant="caption" display="block" gutterBottom>
+                  {product.numReviews} reviews
+                </Typography>
+              </Grid>
+            </Grid>
+            <br></br>
+
+            <Typography variant="body2" display="block" gutterBottom>
+              {product.description}
+            </Typography>
+            <br></br>
+
+            <Button variant="contained" color="default" disableElevation>
+              Add to Cart
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      )}
 
       <br></br>
       <br></br>
