992190d3f8ad0fdc5cd66b14841b22f84b4df025
vmpatoleia@protonmail.com <vmpatoleia@protonmail.com>
2021-04-04 14:12:19 +0100

    Add Quantity select & Add to cart button

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -8,17 +8,57 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProduct } from "../actions/productDetails";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
-export default function ProductScreen({ match }) {
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: "block",
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
+export default function ProductScreen({ history, match }) {
+  const classes = useStyles();
+  const [age, setAge] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const [quantity, setQuantity] = React.useState(0);
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   const dispatch = useDispatch();
 
   const productDetails = useSelector((state) => state.productDetails);
-  console.log(productDetails);
   const { loading, error, product } = productDetails;
 
   useEffect(() => {
     dispatch(fetchProduct(match.params.id));
   }, [dispatch, match]);
+
+  const handleQuantityChange = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${quantity}`);
+  };
 
   return (
     <>
@@ -74,7 +114,7 @@ export default function ProductScreen({ match }) {
               </Grid>
               <Grid item xs={12} sm={4}>
                 <Typography variant="caption" display="block" gutterBottom>
-                  {product.numReviews} reviews
+                  {product.numReviews || 0} reviews
                 </Typography>
               </Grid>
             </Grid>
@@ -83,9 +123,47 @@ export default function ProductScreen({ match }) {
             <Typography variant="body2" display="block" gutterBottom>
               {product.description}
             </Typography>
-            <br></br>
 
-            <Button variant="contained" color="default" disableElevation>
+            {product.countInStock > 0 ? (
+              <>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-controlled-open-select-label">
+                    Quantity
+                  </InputLabel>
+                  <Select
+                    labelId="demo-controlled-open-select-label"
+                    id="demo-controlled-open-select"
+                    open={open}
+                    onClose={handleClose}
+                    onOpen={handleOpen}
+                    value={quantity}
+                    onChange={handleQuantityChange}
+                  >
+                    {[...Array(product.countInStock).keys()].map((num) => (
+                      <MenuItem key={num + 1} value={num + 1}>
+                        {num + 1}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <br></br>
+              </>
+            ) : (
+              <>
+                <br></br>
+                <Typography variant="button" display="block" gutterBottom>
+                  Out of stock
+                </Typography>
+              </>
+            )}
+
+            <Button
+              variant="contained"
+              color="default"
+              disabled={product.countInStock <= 0}
+              disableElevation
+              onClick={addToCartHandler}
+            >
               Add to Cart
             </Button>
           </Grid>
